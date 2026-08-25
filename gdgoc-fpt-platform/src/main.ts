5c7a24ea248340e1c4669e98bb0df0dbbd8ad6b3
chinhan142 <buiphamchinhan@gmail.com>
2026-08-25 12:33:46 +0700

    Setting up prisma schema and config main and app module

--- gdgoc-fpt-platform/src/main.ts
+++ gdgoc-fpt-platform/src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Cors config
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  });
+
+  // Prefix config for API routing
+  app.setGlobalPrefix('api/v1');
+
+  // Setting for validation input of DTO
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('GDGoC FPTU HCMC - Internal Management API')
+    .setDescription('Internal Platform for GDGoC FPT')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}/api/v1`);
+  console.log(
+    `Swagger API Docs available at: http://localhost:${port}/api/docs`,
+  );
 }
 bootstrap();
