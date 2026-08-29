5df3a7eb1986a127118e60377b849cc40b23c4ad
chinhan142 <buiphamchinhan@gmail.com>
2026-08-29 22:10:06 +0700

    [Nhan/29-08] Finish connect drive google apis and auto create folder structure feature

--- backend/src/modules/drive/drive.controller.ts
+++ backend/src/modules/drive/drive.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { DriveService } from './drive.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { CreateEventDto } from './dto/create-event.dto';
+import { Roles } from '../common/decorators/roles.decorator';
+import { Role } from '@prisma/client';
+import { RolesGuard } from '../common/guards/roles.guard';
 
+@ApiTags('Drive')
 @Controller('drive')
 export class DriveController {
   constructor(private readonly driveService: DriveService) {}
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.DEPARTMENT_LEAD, Role.LEAD)
+  @Post('/create-folder')
+  createEventFolder(@Body() dto: CreateEventDto) {
+    return this.driveService.createEventFolder(dto);
+  }
 }
